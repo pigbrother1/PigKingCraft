a261bdccb005cc9200ef23c82f8266890fd460fc
pigbrother1 <2751054328@qq.com>
2016-11-02 22:09:24 +0800

    Revert "按钮的执行函数还有问题，需要sendrpctoserver"
    This reverts commit 421dfab8b4350abf2fe46d7a3da87179331be16d.

--- scripts/init/pkc_choosegroup.lua
+++ scripts/init/pkc_choosegroup.lua
@@ -23,9 +23,8 @@ end
 local function showStartWindow(inst, player)
 	print("name:"..player.name)
 	if player 
-	--and player.components.pkc_group 
-	--and player.components.pkc_group:getChoosen() ~= true		
-	and player.hasChoosen:value() == 0		--这个变量初值为0 
+	and player.components.pkc_group 
+	and player.components.pkc_group:getChoosen() ~= true 
 	then --未选择过阵营时执行
 		makePlayerInvincible(player)
 		local pkc_introduction_screen = require "screens/pkc_introduction_screen"
--- scripts/init/pkc_worldinit.lua
+++ scripts/init/pkc_worldinit.lua
@@ -7,21 +7,8 @@ local TheNet = GLOBAL.TheNet
 local IsServer = TheNet:GetIsServer()
 
 --世界初始化
---@大猪猪 10-31
 AddPrefabPostInit("world", function(inst)
 	if inst then
-		GLOBAL.pkc_setNetvar(inst,{
-			GROUP_BIGPIG_POS_x = {"net_float", 0},
-			GROUP_BIGPIG_POS_z = {"net_float", 0},
-			GROUP_REDPIG_POS_x = {"net_float", 0},
-			GROUP_REDPIG_POS_z = {"net_float", 0},
-			GROUP_LONGPIG_POS_x = {"net_float", 0},
-			GROUP_LONGPIG_POS_z = {"net_float", 0},
-			GROUP_CUIPIG_POS_x = {"net_float", 0},
-			GROUP_CUIPIG_POS_z = {"net_float", 0},
-		})
-		if IsServer then
-			inst:AddComponent("pkc_baseinfo")	--记录阵营的位置
-		end
+	
 	end
 end)
